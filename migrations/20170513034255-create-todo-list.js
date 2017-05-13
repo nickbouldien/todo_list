abe2ee11a63c81cdce40cdf763bcd338ff7d60d2
nick bouldien <nick.bouldien@gmail.com>
2017-05-12 21:28:52 -0700

    start seeds, edit todo/todolist models, make migrations

---
+++ migrations/20170513034255-create-todo-list.js
@@ -0,0 +1,29 @@
+//todo list
+
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('TodoLists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('TodoLists');
+  }
+};
